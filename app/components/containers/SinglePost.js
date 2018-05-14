8e9dc8e3ca5ebcbb30ae6f3d33c4b18400f3115c
kaisinli <kaixinli91@gmail.com>
2018-05-14 03:04:48 -0400

    #comments

--- app/components/containers/SinglePost.js
+++ app/components/containers/SinglePost.js
@@ -6,54 +6,78 @@ import SingleCommentBox from '../presentational/SingleCommentBox';
 import { postNewComment } from '../../reducers/allMessagesReducer';
 
 class SinglePost extends React.Component {
+    constructor(props) {
+        super(props)
+        const id = props.params.id;
+        const post = props.messages[id - 1]
+
+        this.state = {
+            id,
+            title: post.title,
+            message: post.message,
+            timestamp: post.timestamp,
+            user: post.user,
+            comments: post.comments
+        };
+
+        this.submitHandler = this.submitHandler.bind(this);
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+
+        let target = event.target;
+        let comment = target.comment.value,
+            user = target.user.value,
+            id = this.state.id,
+            cmtId = this.state.comments.length ? (this.state.comments.length + 1) : 1;
+        console.log('COMMENT LENGTH', cmtId)
+
+        this.props.postNewComment({ id, user, comment, cmtId });
+
+        this.setState(
+            (prevState, props) => {
+                return { comments: prevState.comments.concat({ id: cmtId, user, comment }) }
+            }
+        );
+    }
+
     render() {
-        let post = this.props.messages[this.props.params.id-1]
-        
+        const post = this.state;
         let hour = post.timestamp.getHours(),
             parsedHour = hour > 12 ? `${hour - 12}` : hour === 0 ? '12' : `${hour}`;
 
         let minute = post.timestamp.getMinutes();
         minute = hour > 12 ? `${minute}pm` : `${minute}am`;
 
-        const submitHandler = (event) => {
-            event.preventDefault();
-            let target = event.target;
-            let comment = target.comment.value,
-                user = target.user.value,
-                id = post.id,
-                cmtId = post.comments.length ? (post.comments.length + 1) : 1;
-
-            this.props.postNewComment({ id, user, comment, cmtId });
-        }
-
         return (
             <div className="single-post-container">
                 <Link to='/'><button className="btn btn-secondary">Back to Posts</button></Link>
-                <div className="single-post-message">
-                    <h3>{post.title}</h3>
-                    <p>By: {post.user} on {parsedHour}:{minute}</p>
-                    <p>{post.message}</p>
+                    <div className = "single-post-message">
+                        <h3>{post.title}</h3>
+                        <p>By: {post.user} on {parsedHour}:{minute}</p>
+                        <p>{post.message}</p>
+                    </div>
+                    <h5>Responses</h5>
+                    {post.comments.length > 0 && post.comments.map(cmt => <SingleCommentBox cmt={cmt} key={cmt.id} />)}
+                    <div>
+                        <form id="new-comment-form" onSubmit={this.submitHandler}>
+                            <label className="required">Message:</label>
+                            <textarea
+                                className="form-control"
+                                name="comment"
+                                type="text"
+                                required />
+                            <label className="required">User:</label>
+                            <input
+                                className="form-control"
+                                name="user"
+                                type="text"
+                                required />
+                            <button className="btn btn-success" type="submit">Post Reply</button>
+                        </form>
+                    </div>
                 </div>
-                <h5>Responses</h5>
-                {post.comments.length > 0 && post.comments.map(cmt => <SingleCommentBox cmt={cmt} key={cmt.id} />)}
-                <div>
-                    <form id="new-comment-form" onSubmit={submitHandler}>
-                        <label className="required">Message:</label>
-                        <textarea
-                            className="form-control"
-                            name="comment"
-                            type="text"
-                            required />
-                        <label className="required">User:</label>
-                        <input
-                            className="form-control"
-                            name="user"
-                            type="text"
-                            required />
-                        <button className="btn btn-success" type="submit">Post Reply</button>
-                    </form>
-                </div>
-            </div>
         )
     }
 }
--- app/components/presentational/SingleMessageBox.js
+++ app/components/presentational/SingleMessageBox.js
@@ -6,7 +6,7 @@ const SingleMessageBox = (props) => {
   const msg = props.msg;
 
   let hour = msg.timestamp.getHours(),
-    parsedHour = hour > 12 ? `${hour - 12}` : hour === 0 ? '12' : `${hour}`;
+      parsedHour = hour > 12 ? `${hour - 12}` : hour === 0 ? '12' : `${hour}`;
 
   let minute = msg.timestamp.getMinutes();
   minute = hour > 12 ? `${minute}pm` : `${minute}am`;
@@ -18,14 +18,12 @@ const SingleMessageBox = (props) => {
         <div className="row">
           <div className="col" id="post-user">
             <p >Posted by: {msg.user}</p>
-          </div>
-          <div className="col" id="post-comments-num">
-            <p>{`${msg.comments.length}`} comments</p>
-          </div>
+          </div> 
+             <div className="col" id="post-user">
+            <p>{`${msg.comments.length} comments`}</p>
+          </div> 
           <div className="col" id="post-time">
-            <p>
-              Last Update: {msg.timestamp.getUTCMonth() + 1}/{msg.timestamp.getUTCDate()}/{JSON.stringify(msg.timestamp.getFullYear()).slice(2)} @ {parsedHour}:{minute}
-            </p>
+            <p>Last Update: {msg.timestamp.getUTCMonth() + 1}/{msg.timestamp.getUTCDate()}/{JSON.stringify(msg.timestamp.getFullYear()).slice(2)} @ {parsedHour}:{minute}</p>
           </div>
         </div>
         <Link to={`/posts/${msg.id}`}>View Post</Link>
